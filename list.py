1ee3d2e8a9a4535bf625f70475b1bdb366ee0ae3
ashikmohamed786 <ashikmohamed7866@gmail.com>
2026-07-01 16:12:30 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,46 @@
+# list1=[1,2,3,4,5]
+# list2=['A','b','c']
+# list3=[1.5,2.5,3.5,4.5,5.5]
+# list4=['yes','no']
+# list1.append(7)
+# print(list1)
+# list1[1]=6
+# print(list1)
+# dict={"name": "Asik" , "reg" : 23243042 , "place" : "sayal" , "num" : "9629118102"}
+# print(dict["name"])
+
+# y=(1,"Asik",5.5)
+# print(y)
+
+# print(len(list1))
+# s=("AsIk")
+# s=s.upper()
+# print(s)
+
+# list1=[1,2,3,4,5,6,7,8,9,7]
+# print(list1)
+# print("max=",max(list1))
+# print("min=",min(list1))
+# average=sum(list1)/len(list1)
+# print("average=",average)
+# list2=list(set(list1))
+# print("remove_duplicate=",list2)
+# list1.sort()
+
+# for i in range(1,5):
+#     if (i==1):
+#         print(True)
+#     else:
+#         print(False)
+# n=[1,2,3,4,5]
+# for j in range:
+#     if n==1:
+#         print
+
+x=[
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]
+print(x)
+
